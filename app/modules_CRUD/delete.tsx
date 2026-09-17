efea29983a87f51a67d7f5212fdfc25991493ccb
jugotoptea-a11y <jugotoptea@gmail.com>
2026-09-17 09:31:39 -0500

    refactor: pass item to handleDeleteTask

--- app/modules_CRUD/delete.tsx
+++ app/modules_CRUD/delete.tsx
@@ -1,13 +1,11 @@
 import styles from "../page.module.css";
- 
-export default function Delete({ id, handleDeleteTask }: any) {
+
+export default function Delete({ item, handleDeleteTask }: any) {
     return (
         <div className={styles.actions}>
-            <button className={styles.deleteButton} onClick={() => handleDeleteTask(id)}>
+            <button className={styles.deleteButton} onClick={() => handleDeleteTask(item)}>
                 Eliminar
             </button>
         </div>
     );
 }
- 
- 
